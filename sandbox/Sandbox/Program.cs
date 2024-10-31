fac9a063d3f0a3c84aebabbf56e336611b56980d
Spencman101 <rey23026@byui.edu>
2024-10-31 09:51:45 -0600

    Added Code to Test writeline

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        // Console.WriteLine("Input your age: "); Dont want to do this, as cursor is on the next line
+        Console.Write("Input your age: ");
+        string userInput = Console.ReadLine();
+        // Console.WriteLine("Hello Sandbox World!");
+
+        // int x = 10;
+
+        // if (x > 15)
+        // {
+        //     Console.WriteLine("Hey Bob");
+        // }
     }
 }
