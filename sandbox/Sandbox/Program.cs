f8f0f1dc7c5c3a219573c16bec0a1ad4b6e13569
Spencman101 <rey23026@byui.edu>
2024-10-31 09:58:10 -0600

    Day 1 Changes Final

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,11 +2,23 @@ using System;
 
 class Program
 {
+    // In C# every program needs an entry point, void is the return type of the function (void = none), Static means that the function can be called without an objects assosiation, Public is repository permissions
     static void Main(string[] args)
     {
-        // Console.WriteLine("Input your age: "); Dont want to do this, as cursor is on the next line
-        Console.Write("Input your age: ");
-        string userInput = Console.ReadLine();
+        Console.WriteLine("Input your age: "); 
+        // Dont want to do this, as cursor is on the next line
+        Console.Write("Input your age: "); 
+        // Console.Write puts the cursor on the same line as it should
+        // Console.Read goes one character at a time
+        
+        // string userInput = Console.ReadLine();
+        // int age = int.Parse(userInput);
+
+        int age = int.Parse(Console.ReadLine());
+        // Variable types must be declared, they cannot change from their type in C#
+        // Prase takes a string and turns it into an integer
+
+        Console.WriteLine($"Your age is: {age}");
         // Console.WriteLine("Hello Sandbox World!");
 
         // int x = 10;
