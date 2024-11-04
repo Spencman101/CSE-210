900854ac095a6780e2373a02bb92efdf232b396a
Spencman101 <rey23026@byui.edu>
2024-11-04 10:01:17 -0700

    In Class/Pre-Program 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What percentage grade did you recieve for your course? ");
+        int score = int.Parse(Console.ReadLine());
+        
+        if (score >= 90)
+            string letter = "A"
+            // Console.WriteLine("The grade you recieved was an A.");
+        else if (score >= 80)
+            Console.WriteLine("The grade you recieved was an B.");
+        else if (score >= 70)
+            Console.WriteLine("The grade you recieved was an C.");
+        else if (score >= 60)
+            Console.WriteLine("The grade you recieved was an D.");
+        else
+            Console.WriteLine("The grade you recieved was an F.");
+        Console.WriteLine($"The grade you recieved was an {letter}");
+
+        if (score >= 70)
+            Console.WriteLine("Congratulations, you passed the class!");
+        else
+            Console.WriteLine("Unfortunately you did not pass, good luck next time!");
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,24 +1,36 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
-    // In C# every program needs an entry point, void is the return type of the function (void = none), Static means that the function can be called without an objects assosiation, Public is repository permissions
+    // In C# every program needs an entry point, void is the return type of the function (void = none), Static means that the function can be called without an objects assosiation and Nonstatic, Public is repository permissions
+    static int AddNumbers(int n1, int n2)
+        {
+            int total = n1 + n2;
+            int total2 = n1 * n2;
+            // Only 1 variable can be returned in C#
+            return total;
+        }
     static void Main(string[] args)
     {
-        Console.WriteLine("Input your age: "); 
+
+        int total = AddNumbers(20, 30);
+        Console.WriteLine(total);
+
+        // Console.WriteLine("Input your age: "); 
         // Dont want to do this, as cursor is on the next line
-        Console.Write("Input your age: "); 
+        // Console.Write("Input your age: "); 
         // Console.Write puts the cursor on the same line as it should
         // Console.Read goes one character at a time
         
         // string userInput = Console.ReadLine();
         // int age = int.Parse(userInput);
 
-        int age = int.Parse(Console.ReadLine());
+        // int age = int.Parse(Console.ReadLine());
         // Variable types must be declared, they cannot change from their type in C#
         // Prase takes a string and turns it into an integer
 
-        Console.WriteLine($"Your age is: {age}");
+        // Console.WriteLine($"Your age is: {age}");
         // Console.WriteLine("Hello Sandbox World!");
 
         // int x = 10;
@@ -27,5 +39,43 @@ class Program
         // {
         //     Console.WriteLine("Hey Bob");
         // }
+
+        // 11/4 In-Class
+        // First part is variable declaration and happens only once, Next is loop condition (conditional) after this it executes the loop, and last is done at the end of the loop and it checks the conditional to see if the loop will run again.
+        // for(int i = 0; i < 10; i++)
+        // {
+        //     Console.WriteLine(i);
+        // }
+
+        // int age = -1;
+
+        // while (age < 0 || age > 125)
+        // {
+        //     Console.Write("Please enter your age: (0-125): ");
+        //     age = int.Parse(Console.ReadLine());
+        //     Console.WriteLine($"Your age is: {age}");
+        // }
+
+        // Must be declared before do while loop as it would only exist in the loop and couldn't carry out to while loop as it is outside the code block.
+        // int age;
+        // Do while loops are garunteed to be executed once, so you dont need a entrance strategy (Dont Need age = 1)
+        // do
+        // {
+        //     Console.Write("Please enter your age: (0-125): ");
+        //     age = int.Parse(Console.ReadLine());
+        //     Console.WriteLine($"Your age is: {age}");
+        // }while (age < 0 || age > 125);
+
+        // List <string> myColors = new List<string>();
+
+        // myColors.Add("Red");
+        // myColors.Add("Green");
+        // myColors.Add("Blue");
+
+        // foreach(string color in myColors)
+        // {
+        //     Console.WriteLine(color);
+        // }
+
     }
 }
