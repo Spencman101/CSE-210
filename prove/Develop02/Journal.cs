e8cdc04bdb924e76eea1553eb02e93517fb246e8
Spencman101 <rey23026@byui.edu>
2024-11-13 10:02:09 -0700

    In-Class Code Start

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Journal 
+{
+    private List<string> _entries;
+    private string _prompt;
+    private int _wordcount;
+
+    static void DisplayAllEntries()
+    {
+        
+    }
+    static void WriteEntriesToFile(fileName)
+    {
+
+    }
+    static void ReadEntriesFromFile(filename)
+    {
+
+    }
+}
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -2,8 +2,26 @@ using System;
 
 class Program
 {
+    private string fileName;
+    
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop02 World!");
+    }
+    static void GetSelection()
+    {
+        int selection;
+        do
+        {
+            selection = (int.Parse(Console.ReadLine()));
+        }while (selection != 5);
+    }
+    static void GetFileName()
+    {
+        
+    }
+    static void DisplayMenu()
+    {
+        Console.WriteLine("Please select one of the following chocies:\n1. Write \n2. Display \n3. Load \n4. Save \n5. Quit \nWhat would you like to do?");
     }
 }
