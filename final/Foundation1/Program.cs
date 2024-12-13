eeaf717cc10bdbad1b9f80fd5c157b06d6995ce8
Spencman101 <rey23026@byui.edu>
2024-12-13 00:50:40 -0700

    Final Coding Apart from videos/comment inserts

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
+    public List<Video> Videos;
+
+    static Video video;
+
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation1 World!");
+        CreateVideo();
+        CreateComment();
+        
+    }
+
+    public void DisplayVideo()
+    {
+        foreach (var video in Videos)
+        {
+            Console.WriteLine(video);
+        }
+    }
+
+    public static void CreateComment()
+    {
+        Comment com1 = new Comment();
+        com1._commenter = "PixelPirate42";
+        com1._text = "This video is like a treasure map! I keep finding gems every time I rewatch it!";
+        video.StoreComments(com1);
+
+        Comment com2 = new Comment();
+        com2._commenter = "BananaramaKing";
+        com2._text = "Why isn’t anyone talking about the random banana at 3:42? Absolute gold.";
+
+        Comment com3 = new Comment();
+        com3._commenter = "QuantumKoala99";
+        com3._text = "The editing on this video is smoother than my attempts at parallel parking.";
+
+    }
+
+    public static void CreateVideo()
+    {
+        video = new Video("The Science of Black Holes Explained", "AstroNerd101", 720);
     }
 }
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -8,24 +8,26 @@ class Video
 
     public int _seconds;
 
-    public List<string> Comments;
+    public List<Comment> _comments;
 
     public Video(string title, string author, int seconds)
     {
         _title = title;
         _author = author;
         _seconds = seconds;
-        List<string> comments;
-        comments = new List<string>();
+        _comments = new List<Comment>();
     }
     
     public void StoreComments(Comment com)
     {
-        comments.Add(com);
+        _comments.Add(com);
     }
 
     public void DisplayComments()
     {
-
+        foreach (var comment in _comments)
+        {
+            Console.WriteLine(comment);
+        }
     }
 }
